3cdcaa29288f73b26bd94f9ee07b3f9d56840f68
Enukio <38923168+Enukio@users.noreply.github.com>
2024-11-16 21:58:51 +0700

    Update logger.py

--- bot/utils/logger.py
+++ bot/utils/logger.py
@@ -12,11 +12,4 @@ logger.add(
 
 logger = logger.opt(colors=True)
 
-if __name__ == "__main__":
-    df = load_data("data.parquet", columns=["count"])
-    logger.info("Data Loaded")
-
-    logger.info("Loading Model")
-    nlp = spacy.load("en_core_web_lg", disable=["ner"])
-    logger.info(f"loaded model. Pipeline: {nlp.pipe_names}")
-    logger.error(f"Failed to configure logger: {e}")
+logger.error(f"Failed to configure logger: {e}")
