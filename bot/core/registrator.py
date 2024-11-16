8c278246ec61ad0211216c3c9fd6f9d7b0eaf822
Enukio <38923168+Enukio@users.noreply.github.com>
2024-11-16 22:21:31 +0700

    Update registrator.py

--- bot/core/registrator.py
+++ bot/core/registrator.py
@@ -1,4 +1,5 @@
-from pyrogram import Client
+
+from pyrogram import Client, errors
 
 from bot.config import settings
 from bot.utils import logger
@@ -11,19 +12,34 @@ async def register_sessions() -> None:
     if not API_ID or not API_HASH:
         raise ValueError("API_ID and API_HASH not found in the .env file.")
 
-    session_name = input('\nEnter the session name (press Enter to exit): ')
+    while True:
+        session_name = input("\nEnter the session name (or type 'exit' to quit): ").strip()
 
-    if not session_name:
-        return None
+        if session_name.lower() == 'exit':
+            print("Exiting session registration.")
+            return
 
-    session = Client(
-        name=session_name,
-        api_id=API_ID,
-        api_hash=API_HASH,
-        workdir="sessions/"
-    )
+        if not session_name:
+            print("Session name cannot be empty. Please try again.")
+            continue
 
-    async with session:
-        user_data = await session.get_me()
+        session = Client(
+            name=session_name,
+            api_id=API_ID,
+            api_hash=API_HASH,
+            workdir="sessions/"
+        )
 
-    logger.success(f'Session added successfully @{user_data.username} | {user_data.first_name} {user_data.last_name}')
+        try:
+            async with session:
+                user_data = await session.get_me()
+                logger.success(f'Session added successfully @{user_data.username} | '
+                               f'{user_data.first_name} {user_data.last_name}')
+                break
+        except errors.AccessTokenInvalid:
+            logger.error("Invalid API ID or HASH. Please check your .env configuration.")
+            break
+        except errors.RPCError as rpc_error:
+            logger.error(f"An RPC error occurred: {rpc_error}")
+        except Exception as e:
+            logger.error(f"An unexpected error occurred: {e}")
