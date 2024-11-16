bea0ac8ec5f742ad6f871185f5f3f0062dccd4d6
Enukio <38923168+Enukio@users.noreply.github.com>
2024-11-16 21:54:00 +0700

    Update logger.py

--- bot/utils/logger.py
+++ bot/utils/logger.py
@@ -3,8 +3,13 @@ from loguru import logger
 
 
 logger.remove()
-logger.add(sink=sys.stdout, format="<r>[Not pixel]</r> | <white>{time:YYYY-MM-DD HH:mm:ss}</white>"
-                                   " | <level>{level: <8}</level>"
-                                   " | <cyan><b>{line}</b></cyan>"
-                                   " - <white><b>{message}</b></white>")
+logger.add(
+    sink=sys.stdout, 
+    format="<r>[Not pixel]</r> | <white>{time:YYYY-MM-DD HH:mm:ss}</white>"
+           " | <level>{level: <8}</level>"
+           " | <cyan><b>{line}</b></cyan>"
+           " - <white><b>{message}</b></white>")
 logger = logger.opt(colors=True)
+
+except Exception as e:
+    logger.error(f"Failed to configure logger: {e}")
