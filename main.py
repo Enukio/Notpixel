016243ee696b91f691e7a34ea8e95aaae81d663b
Enukio <38923168+Enukio@users.noreply.github.com>
2024-11-16 22:39:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from contextlib import suppress
 
 from bot.utils.launcher import process
 
+
 async def main():
     await process()
+
 
 if __name__ == '__main__':
     with suppress(KeyboardInterrupt):
