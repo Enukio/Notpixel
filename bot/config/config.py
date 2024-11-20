f9ee0be45e6069e69a17a68e14709bd89ad1b139
Enukio <38923168+Enukio@users.noreply.github.com>
2024-11-21 04:39:16 +0700

    Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -4,11 +4,11 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True)
 
-    API_ID: int
-    API_HASH: str
+    API_ID: int = 0
+    API_HASH: str = ""
 
 
-    REF_LINK: str = "https://t.me/notpixel/app?startapp"
+    REF_LINK: str = ""
     AUTO_UPGRADE_PAINT_REWARD: bool = True
     AUTO_UPGRADE_RECHARGE_SPEED:bool = True
     AUTO_UPGRADE_RECHARGE_ENERGY:bool = True
